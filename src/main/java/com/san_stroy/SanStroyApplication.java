4a8f3a7cdb4706c96233c83e80ac4a4a4e7a17bf
Syrgak13 <syrgaktabyldiev13@gmail.com>
2025-05-15 17:52:24 +0600

    First commit

---
+++ src/main/java/com/san_stroy/SanStroyApplication.java
@@ -0,0 +1,13 @@
+package com.san_stroy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanStroyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanStroyApplication.class, args);
+    }
+
+}
